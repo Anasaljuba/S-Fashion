707ea217acf8ff85e743ba312adbf76cd9618763
Anas Saad <anassaad@Anass-MacBook-Air.local>
2022-01-16 19:10:58 +0300

    Push 1.1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
+      
+
+        <h1>S Fashion</h1>
+        <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+        <p>Fashion is best defined simply as the style <br/>or styles of clothing and accessories worn<br/>at any given timeby groups of people.</p>
         
-       
+        <img src="https://wwd.com/wp-content/uploads/2020/04/shutterstock_editorial_10559571aw.jpg"alt="google advertising" className='mainimg'/>
       </header>
     </div>
   );
